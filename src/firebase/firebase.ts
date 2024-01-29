9748dae14772944eb4ab650b93b011184ffd4077
junmiLee <junmi950918@gmail.com>
2024-01-29 22:39:07 +0900

    omment : 주석 추가

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,6 +1,11 @@
-import { getAuth } from "firebase/auth";
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAhMoM0dkz4Zr55S0448Q4L-tO53Q25K68",
   authDomain: "myidol-b0c72.firebaseapp.com",
@@ -13,5 +18,3 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-// React에서 정상적으로 동작하는 지 확인하기 위해서 임시로 export 시켜줍니다. app이 정상적으로 출력되는 것을 확인하고 나면, 지워줍니다.
