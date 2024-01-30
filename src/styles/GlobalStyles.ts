d9d66d3fc61b9b33c45b1e8f1ba7662271c6a124
junmiLee <junmi950918@gmail.com>
2024-01-31 03:17:46 +0900

    feat : globalStyle 추가

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -66,6 +66,30 @@ const GlobalStyles = createGlobalStyle`
     font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
       monospace;
   }
+
+  a{
+    text-decoration: none;
+    color: inherit;
+}
+*{
+    box-sizing: border-box;
+}
+input, textarea { 
+  -moz-user-select: auto;
+  -webkit-user-select: auto;
+  -ms-user-select: auto;
+  user-select: auto;
+}
+input:focus {
+  outline: none;
+}
+
+button {
+  border: none;
+  background: none;
+  padding: 0;
+  cursor: pointer;
+}
 `;
 
 export default GlobalStyles;
