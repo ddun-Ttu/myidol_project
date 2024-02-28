e0119d7039ed77317b803b06ddcf19a7430711e5
junmiLee <junmi950918@gmail.com>
2024-02-28 21:59:44 +0900

    feat : 결제창 퍼블리싱

---
+++ src/components/CheckoutComponent/CheckoutComponent.tsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import MainNav from "../CommonComponent/MainNav/MainNav";
+
+const CheckoutComponent = () => {
+
+    
+  return (
+    <>
+      <MainNav />
+    </>
+  );
+};
+
+export default CheckoutComponent;
---
+++ src/pages/Checkout/Checkout.tsx
@@ -0,0 +1,12 @@
+import CheckoutComponent from "src/components/CheckoutComponent/CheckoutComponent";
+
+
+const Checkout = () => {
+  return (
+    <>
+    <CheckoutComponent />
+    </>
+  );
+};
+
+export default Checkout;
